0f051e346510c14e50afaeca07ac12efcb07e010
sunjiajie <sunjiajie@kuaishou.com>
2021-08-04 20:51:47 +0800

    chore: 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = object[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
@@ -15,14 +17,14 @@ export const cleanObject = (object) => {
 
 // 自定义hook
 // 加载页面执行一次
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
 // 防抖
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
