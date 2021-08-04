7225ff65fbe7fb9cf6507eb4a8d2e5c0f3c1da7a
sunjiajie <sunjiajie@kuaishou.com>
2021-08-04 17:59:51 +0800

    feat: 使用状态提升分享组件状态

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,21 +1,4 @@
-import { useState, useEffect } from "react"
-
-export const SearchPanel = () => {
-    const [param, setParam] = useState({
-        name: '',
-        personId: ''
-    })
-    const [users, setUsers] = useState([]);
-    const [list, setList] = useState([]);
-
-    useEffect(() => {
-        fetch('').then(async response => {
-            if(response.ok){
-                setList(await response.json);
-            }
-        })
-    }, [param]);
-
+export const SearchPanel = ({users, param, setParam}) => {
     return <form action="">
         <div>
             <input type="text" value={param.name} onChange={evt => setParam({
@@ -28,7 +11,7 @@ export const SearchPanel = () => {
             })}>
                 <option value={''}>负责人</option>
                 {
-                    users.map(user => <option value={user.id}>{user.name}</option>)
+                    users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
                 }
             </select>
         </div>
