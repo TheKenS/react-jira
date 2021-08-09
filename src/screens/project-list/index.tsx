5ddde0c282644297a4d31db24cca45d700077b1d
sunjiajie <sunjiajie@kuaishou.com>
2021-08-09 12:01:05 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { cleanObject, useMount, useDebounce } from "utils";
 import * as qs from "qs";
+import { useHttp } from "utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -13,23 +14,14 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
   const debouncedParam = useDebounce(param, 200);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users", { data: cleanObject(debouncedParam) }).then(setUsers);
   });
 
   return (
